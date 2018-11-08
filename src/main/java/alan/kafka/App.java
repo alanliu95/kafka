3b8d49465e91b4933fac33271a60e9dc023f6a39
alan <438162498@qq.com>
2018-11-08 21:48:33 +0800

    first commit from local

---
+++ src/main/java/alan/kafka/App.java
@@ -0,0 +1,13 @@
+package alan.kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
