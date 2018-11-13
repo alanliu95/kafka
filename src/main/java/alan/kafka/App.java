4fcdc15eb598bce0e5bef82c7202b168580985ea
alan <438162498@qq.com>
2018-11-13 22:25:40 +0800

    modified App.java

--- src/main/java/alan/kafka/App.java
+++ src/main/java/alan/kafka/App.java
@@ -1,5 +1,9 @@
 package alan.kafka;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	 Properties props = new Properties();
+    	 props.put("bootstrap.servers", "alannnn.tpddns.cn:9092");
+    	 props.put("acks", "all");
+    	 props.put("retries", 0);
+    	 props.put("batch.size", 16384);
+    	 props.put("linger.ms", 1);
+    	 props.put("buffer.memory", 33554432);
+    	 props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    	 props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+    	 Producer<String, String> producer = new KafkaProducer<>(props);
+    	 for (int i = 0; i < 10; i++)
+    	     producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)));
+
+    	 producer.close();
     }
 }
