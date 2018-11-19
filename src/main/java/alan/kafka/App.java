64e2d80ae675f9174c07fd8fd467cd95bbaf26ef
alan <438162498@qq.com>
2018-11-19 21:56:29 +0800

    add SysStatus.java

--- src/main/java/alan/kafka/App.java
+++ src/main/java/alan/kafka/App.java
@@ -24,8 +24,10 @@ public class App
 
     	 Producer<String, String> producer = new KafkaProducer<>(props);
     	 for (int i = 0; i < 10; i++)
-    	     producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)));
-
+    	     producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i+2000)));
     	 producer.close();
     }
 }
+
+
+
